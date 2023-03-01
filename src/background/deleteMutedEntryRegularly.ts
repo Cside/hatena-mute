c9c19d048854d40d1350a0b002ff443ef4d2e934
Cside <cside.story@gmail.com>
2023-03-01 16:19:41 +0900

    fix comment

--- src/background/deleteMutedEntryRegularly.ts
+++ src/background/deleteMutedEntryRegularly.ts
@@ -1,6 +1,6 @@
 import { userOption } from '../userOption';
-const INTERVAL = 24 * 60; // minutes
-const OLDER_THAN = 1 * 24 * 60; // minutes
+const INTERVAL = 24 * 60; // minutes (24 hours)
+const OLDER_THAN = 4 * 24 * 60; // minutes (4 days)
 const ALARM_NAME = 'delete-muted-entries';
 
 chrome.alarms.onAlarm.addListener(async () => {
