39c570fca92b4b3ac106cf3bdd544c932e5fd1ea
Cside <cside.story@gmail.com>
2023-03-10 14:06:54 +0900

    add id

--- scripts/manifest-firefox.js
+++ scripts/manifest-firefox.js
@@ -5,5 +5,10 @@ manifest.background = { page: 'background.html' };
 for (const resource of manifest.web_accessible_resources) {
   delete resource.use_dynamic_url;
 }
+manifest.browser_specific_settings = {
+  gecko: {
+    id: 'hatena-mute@github.com',
+  },
+};
 
 console.info(JSON.stringify(manifest, null, 2));
