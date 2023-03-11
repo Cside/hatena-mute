eb30ad5c615ac702e65c73f13c1b69d72c2ca231
Cside <cside.story@gmail.com>
2023-03-11 12:25:09 +0900

    request permission 機能

--- src/popup/components/App/index.tsx
+++ src/popup/components/App/index.tsx
@@ -1,12 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import { LighteningOptions } from '../LightningOptions';
 import { MuteOptions } from '../MuteOptions';
+import { PermissionRequest } from '../PermissionRequest';
 import './bootstrap.scss';
 import './styles.scss';
 
 export const App = () => {
   return (
     <Container fluid="sm" className="m-1">
+      <PermissionRequest />
       <MuteOptions />
       <LighteningOptions />
     </Container>
---
+++ src/popup/components/PermissionRequest/index.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import { getOrigin } from '../../../utils';
+
+const PERMISSION = {
+  origins: [getOrigin()],
+};
+
+export const PermissionRequest = () => {
+  const [hasPermitted, setHasPermitted] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setHasPermitted(await chrome.permissions.contains(PERMISSION));
+    })();
+  }, []);
+
+  return hasPermitted ? null : (
+    <Alert variant="danger" className="mt-2">
+      <Alert.Heading>初期設定が未完了です</Alert.Heading>
+      <Alert.Link
+        href="#"
+        onClick={async (
+          event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+        ) => {
+          setHasPermitted(await chrome.permissions.request(PERMISSION));
+          event.preventDefault();
+        }}
+      >
+        ここをクリックして、この拡張が b.hatena.ne.jp
+        にアクセスする権限を許可してください。
+      </Alert.Link>
+    </Alert>
+  );
+};
