33e8c3cc5ded9c49c687d2c6970d87fac65bc54a
Cside <cside.story@gmail.com>
2023-03-13 16:29:11 +0900

    release:firefox で source code の zip も作る

--- scripts/build-firefox.sh
+++ scripts/build-firefox.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -eux;
 
-pnpm run build;
-cp -r dist dist_firefox;
-node ./scripts/manifest-firefox.js > dist_firefox/manifest.json;
+IS_FIREFOX=true pnpm run build;
+mv dist dist_firefox;
+cat dist_firefox/manifest.json | node ./scripts/manifest-firefox.js > manifest_firefox.json;
+mv manifest_firefox.json dist_firefox/manifest.json
 cp background.html dist_firefox/background.html;
