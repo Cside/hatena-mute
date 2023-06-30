b96c6b62a69451620da7f41478d56bee8593c49b
Cside <cside.story@gmail.com>
2023-06-30 17:37:54 +0900

    fix scripts

---
+++ scripts/zip-firefox.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eux
+
+pnpm install
+pnpm run build:firefox
+
+version=$(jq -r .version <package.json | sed 's/\./-/g')
+
+(
+    cd dist-firefox
+    zip -r -FS ../dist-firefox-"${version}".zip *
+)
+
+rm -rf source-code source-code.zip
+mkdir source-code
+for file in $(git ls-files . | sed 's|/.*||' | uniq); do
+    cp -r "$file" source-code/
+done
+
+zip -r source-code-"${version}".zip source-code
+rm -rf source-code
+
+rm -rf dist-firefox
